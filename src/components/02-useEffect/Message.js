e5d3706e9ed81793d3dfdb750166cd09b19e4176
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-02 14:47:34 -0500

    109. useEffect - Precauciones, limpiar componente

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,27 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function Message(props) {
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+  const { x, y } = coords;
+
   useEffect(() => {
-    console.log("componente montado");
+    const mouseMove = (e) => {
+      const coords = { x: e.x, y: e.y };
+      setCoords(coords);
+    };
+    window.addEventListener("mousemove", mouseMove);
+
     return () => {
-      console.log("componente desmontado");
+      window.removeEventListener("mousemove", mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h3>Genial!</h3>
+      <p>
+        X:{x}, Y: {y}
+      </p>
     </div>
   );
 }
