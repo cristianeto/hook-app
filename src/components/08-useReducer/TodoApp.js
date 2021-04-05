2fea3dd7dbaee273a54829cef443a90e5fed48cf
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-05 10:48:03 -0500

    127. Agregar un nuevo TODO

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,6 +1,7 @@
 import React, { useReducer } from "react";
-import "./styles.css";
 import { todoReducer } from "./todoReducer";
+
+import "./styles.css";
 
 const initialState = [
   {
@@ -11,18 +12,61 @@ const initialState = [
 ];
 
 const TodoApp = () => {
-  const [todos] = useReducer(todoReducer, initialState);
+  const [todos, dispatch] = useReducer(todoReducer, initialState);
 
   console.log(todos);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: "New todo",
+      done: false,
+    };
+    const action = {
+      type: "add",
+      payload: newTodo,
+    };
+    dispatch(action);
+  };
+
   return (
     <div>
-      <h1>TodoApp</h1>
+      <h1>TodoApp ({todos.length})</h1>
       <hr />
-      <ul>
-        <li>Hello</li>
-        <li>World</li>
-        <li>new hello world</li>
-      </ul>
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, index) => (
+              <li key={todo.id} className="list-group-item">
+                <p className="text-center">
+                  {index + 1}. {todo.desc}
+                </p>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-5">
+          <h4>Agregar TODO</h4>
+          <hr />
+          <form onSubmit={handleSubmit}>
+            <input
+              autoComplete="off"
+              className="form-control"
+              name="description"
+              placeholder="description"
+              type="text"
+            />
+            <button
+              type="submit"
+              className="btn btn-outline-primary mt-1 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
