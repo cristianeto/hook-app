650b1c0c64fe98aea3182d10bf50fbc2df5922b4
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-05 10:06:38 -0500

    125. useReducer - Todo List

---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,11 @@
+export const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    // case "add":
+    //   [...state];
+
+    //   break;
+
+    default:
+      return state;
+  }
+};
