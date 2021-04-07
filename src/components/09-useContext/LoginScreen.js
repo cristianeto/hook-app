7a632229dde5783d5cc2d2c339155ca730deb782
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-07 12:16:22 -0500

    11. Context 141. useContext

--- src/components/09-useContext/LoginScreen.js
+++ src/components/09-useContext/LoginScreen.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "./UserContext";
 
 const LoginScreen = () => {
+  const { user, setUser } = useContext(UserContext);
   return (
     <div>
       <h1>LoginScreen</h1>
       <hr />
+      <button
+        className="btn btn-primary"
+        onClick={() => setUser({ id: "1234", name: "Cristian Guamán" })}
+      >
+        Login
+      </button>
     </div>
   );
 };
