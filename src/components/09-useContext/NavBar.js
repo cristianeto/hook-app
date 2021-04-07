841fa9c6459bba1051ce719268c3adb90cfb0b03
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-07 11:07:43 -0500

    Actualizacion de navbar, nada importante

--- src/components/09-useContext/NavBar.js
+++ src/components/09-useContext/NavBar.js
@@ -5,9 +5,9 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
       <div className="container-fluid">
-        <NavLink to="/" className="navbar-brand">
+        <Link to="/" className="navbar-brand">
           UseContext
-        </NavLink>
+        </Link>
 
         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div className="navbar-nav">
