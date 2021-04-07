aca3396b0cd959ce85b84cb6c3aadefeee335477
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-07 15:31:56 -0500

    12. Tests 147. Ejecutar funciones del customHook dentro de las pruebas

--- src/tests/hooks/useCounter.test.js
+++ src/tests/hooks/useCounter.test.js
@@ -1,10 +1,51 @@
-import React from "react";
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useCounter } from "../../hooks/useCounter";
 
 describe("Testing to useCounter.js", () => {
   test("should return default values", () => {
     const { result } = renderHook(() => useCounter());
-    console.log(result);
+    expect(result.current.counter).toBe(10);
+    expect(typeof result.current.increment).toBe("function");
+    expect(typeof result.current.decrement).toBe("function");
+    expect(typeof result.current.reset).toBe("function");
+  });
+
+  test("should return the counter with 100 as a initialized value", () => {
+    const { result } = renderHook(() => useCounter(100));
+    expect(result.current.counter).toBe(100);
+  });
+
+  test("should increment the counter in +1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { increment } = result.current;
+
+    act(() => {
+      increment();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(101);
+  });
+
+  test("should decrement the counter in -1", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement } = result.current;
+
+    act(() => {
+      decrement();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(99);
+  });
+
+  test("should reset the counter to the initialized value", () => {
+    const { result } = renderHook(() => useCounter(100));
+    const { decrement, reset } = result.current;
+
+    act(() => {
+      decrement();
+      reset();
+    });
+    const { counter } = result.current;
+    expect(counter).toBe(100);
   });
 });
