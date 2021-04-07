865855b21704247e9d374e11e376a732cf700b57
Cristian Guaman <cristian.guaman@icloud.com>
2021-04-07 11:36:29 -0500

    11. Context 140. CreateContext y useContext

--- src/components/09-useContext/MainApp.js
+++ src/components/09-useContext/MainApp.js
@@ -1,9 +1,20 @@
 import React from "react";
 
 import AppRouter from "./AppRouter";
+import { UserContext } from "./UserContext";
 
 const MainApp = () => {
-  return <AppRouter />;
+  const user = {
+    id: 1234,
+    name: "Fernando",
+    email: "fernando.herrera@gmail.com",
+  };
+
+  return (
+    <UserContext.Provider value={user}>
+      <AppRouter />
+    </UserContext.Provider>
+  );
 };
 
 export default MainApp;
